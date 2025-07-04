be80c2d9562746405636e5fd5b68c3debd6b968c
anthony-murè <tony.mure99@gmail.com>
2025-07-04 16:43:12 +0200

    [add]-listEmail-party

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*Mail
+Crea una lista di email di invitati ad una festa.
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
+Si può fare? Certo che si basta ragionare un po’.
+Nota:
+Non è necessario provvedere alla validazione delle email*/
+
+//creo una lista di email di invitati a duna festa//
+
+const listEmail = ["ironman@gmail.com","walter.white@gmail.com","peter.parker@gmail.com","gerald.di.rivia@gmail.com","monkey.d.luffy@gmail.com","nico.robin@gmail.com","mikasa.ackerman@gmail.com","nezuko.kamado.@gmail.com","fujiko.mine@gmail.com","bulma.brief@gmail.com"];
